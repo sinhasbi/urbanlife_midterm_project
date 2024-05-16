51d123c7f1adaa0ad23150d2d2400fc040cfb8c3
Hasbi <sinhasbi123@gmail.com>
2024-05-16 20:00:09 +0800

    20240516 SESSEION的修改

--- member_pages/doRegister.php
+++ member_pages/doRegister.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once("../db_connect.php");
+require_once ("../db_connect.php");
 
 if (!isset($_POST["account"])) {
     die("請循正常管道進入此頁面");
@@ -16,26 +16,19 @@ $phone = trim($_POST["phone"]);
 $address = trim($_POST["address"]);
 $creditcard = trim($_POST["creditcard"]);
 
-
-
+// 清空错误信息
+$_SESSION["error"] = [];
 
 // if (empty($name) || empty($account) || empty($password) || empty($email) || empty($birthday) || empty($phone) || empty($address) || empty($creditcard)) {
 //     die("請輸入必填欄位");
-    
 //     exit;
-    
 // }
 
 if (empty($name)) {
-    $_SESSION["error"]["message"] = "請輸入名稱";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["name"] = "請輸入名稱";
 }
-
 if (empty($account)) {
-    $_SESSION["error"]["message"] = "請輸入帳號";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["account"] = "請輸入帳號";
 }
 
 $checkAccount = "SELECT * FROM user WHERE account='$account'";
@@ -43,81 +36,61 @@ $result = $conn->query($checkAccount);
 $accountExist = $result->num_rows;
 
 if ($accountExist != 0) {
-    $_SESSION["error"]["message"] = "帳號已存在";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["account"] = "帳號已存在";
 }
-
 if (empty($password)) {
-    $_SESSION["error"]["message"] = "請輸入密碼";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["password"] = "請輸入密碼";
 }
 if ($password != $repassword) {
-    $_SESSION["error"]["message"] = "密碼輸入不一致";
-    header("location:../member_pages/register.php");
-    exit; 
+    $_SESSION["error"]["repassword"] = "密碼輸入不一致";
 }
 if (empty($email)) {
-    $_SESSION["error"]["message"] = "請輸入信箱";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["email"] = "請輸入信箱";
 }
 if (empty($birthday)) {
-    $_SESSION["error"]["message"] = "請輸入生日";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["birthday"] = "請輸入生日";
 }
 if (empty($phone)) {
-    $_SESSION["error"]["message"] = "請輸入電話號碼";
-    header("location:../member_pages/register.php");
-    exit;
+    $_SESSION["error"]["phone"] = "請輸入電話號碼";
+}
+if (empty($address)) {
+    $_SESSION["error"]["address"] = "請輸入信用卡號";
 }
 if (empty($creditcard)) {
-    $_SESSION["error"]["message"] = "請輸入密碼";
-    header("location:../member_pages/register.php");
+    $_SESSION["error"]["creditcard"] = "請輸入信用卡號";
+}
+
+// 如果有任何错误信息，重定向回表单页面
+if (!empty($_SESSION["error"])) {
+    header("Location: ../member_pages/register.php");
     exit;
 }
 
-
-
 $now = date('Y-m-d H:i:s');
-//簡單的加密方法
+// 簡單的加密方法
 // $password = md5($password);
 
-
 if ($_FILES["image"]["error"] == 0) {
-
     $filetime = time();
     $fileExt = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
     $filename = $filetime . "." . $fileExt;
 
-
     if (move_uploaded_file($_FILES["image"]["tmp_name"], "../member_images/" . $filename)) {
-        //上傳到資料庫
-        
-        $now = date('Y-m-d H:i:s');
+        // 上傳到資料庫
+        $sql = "INSERT INTO user (name, account, password, email, birthday, phone, address, credit_number, created_at, valid, img)
+                VALUES ('$name','$account','$password','$email','$birthday','$phone','$address','$creditcard','$now',1,'$filename')";
 
-        $sql = "INSERT INTO user (name, account,password,email,birthday,phone,address,credit_number,created_at,valid,img)
-        VALUES ('$name','$account','$password','$email','$birthday','$phone','$address','$creditcard','$now',1,'$filename')";
-
-        // $sql = "INSERT INTO images (name, filename, created_at)
-        //         VALUES ('$name', '$filename','$now')";
         if ($conn->query($sql)) {
             echo "新增資料完成";
         } else {
             echo "新增資料錯誤: " . $conn->error;
         }
-
-
-        echo "upload success!";
-
     } else {
-        echo "upload failed!". $conn->error;
+        echo "upload failed!" . $conn->error;
     }
-
 }
 
-
 $conn->close();
-header("location: ../member_pages/member.php?search=$name");
+header("Location: ../member_pages/member.php?search=$name");
+exit;
+?>
--- member_pages/register.php
+++ member_pages/register.php
@@ -1,12 +1,7 @@
 <?php
 session_start();
-require_once("../db_connect.php");
-
-$sql = "SELECT img FROM user ORDER By id DESC";
-$result = $conn->query($sql);
-$rows = $result->fetch_all(MYSQLI_ASSOC);
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,86 +34,106 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
       height: var(--width);
       overflow: hidden;
       position: relative;
+    }
 
-      img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
+    .previewimage img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
 
-      .deletebtn {
-        position: absolute;
-        left: 150px;
-        bottom: 0;
-        display: none;
-
-      }
-
+    .deletebtn {
+      position: absolute;
+      left: 150px;
+      bottom: 0;
+      display: none;
     }
   </style>
 </head>
 
-<body class="g-sidenav-show   bg-gray-100">
-
-  <main class="main-content  mt-0 ">
-
+<body class="g-sidenav-show bg-gray-100">
+  <main class="main-content mt-0">
     <form action="doRegister.php" method="post" enctype="multipart/form-data" novalidate>
       <div class="container">
         <div class="py-2">
-          <a class="btn btn-primary " href="../member_pages/member.php" role="button"><i class="fa-solid fa-angles-left fa-fw"></i>回到會員列表</a>
+          <a class="btn btn-primary" href="../member_pages/member.php" role="button"><i
+              class="fa-solid fa-angles-left fa-fw"></i>回到會員列表</a>
         </div>
 
         <h1 class="text-center my-4">新增使用者</h1>
         <div class="row justify-content-center">
           <div class="col-lg-6 col-md-8 d-flex justify-content-center mt-8">
-            <div class="previewimage border ">
+            <div class="previewimage border">
               <img alt="圖片預覽">
               <div>
                 <button class="btn btn-warning deletebtn" type="hidden" id="deletebtn">更換圖片</button>
               </div>
-
             </div>
-
           </div>
 
           <div class="col-lg-6 col-md-8">
-
-
             <div class="mb-2">
               <label for="" class="form-label">名稱</label>
               <input type="text" class="form-control" name="name">
+              <?php if (isset($_SESSION["error"]["name"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["name"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">帳號</label>
               <input type="text" class="form-control" name="account">
+              <?php if (isset($_SESSION["error"]["account"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["account"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">密碼</label>
               <input type="password" class="form-control" name="password">
+              <?php if (isset($_SESSION["error"]["password"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["password"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">重新輸入密碼</label>
               <input type="password" class="form-control" name="repassword">
+              <?php if (isset($_SESSION["error"]["repassword"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["repassword"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">信箱</label>
               <input type="email" class="form-control" name="email">
+              <?php if (isset($_SESSION["error"]["email"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["email"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">生日</label>
               <input type="date" class="form-control" name="birthday">
+              <?php if (isset($_SESSION["error"]["birthday"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["birthday"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">電話號碼</label>
               <input type="text" class="form-control" name="phone">
+              <?php if (isset($_SESSION["error"]["phone"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["phone"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">地址</label>
               <input type="text" class="form-control" name="address">
+              <?php if (isset($_SESSION["error"]["address"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["address"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">信用卡號</label>
               <input type="text" class="form-control" name="creditcard">
+              <?php if (isset($_SESSION["error"]["creditcard"])): ?>
+                <div class="text-danger"><?= $_SESSION["error"]["creditcard"] ?></div>
+              <?php endif; ?>
             </div>
             <div class="mb-2">
               <label for="" class="form-label">圖片</label>
@@ -127,197 +142,38 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
             <div class="text-center mt-5">
               <button class="btn btn-primary px-10" type="submit">新增使用者</button>
             </div>
-            <?php if (isset($_SESSION["error"]["message"])) : ?>
-              <div class="">
-                <div class="text-danger text-center">
-                  <?= $_SESSION["error"]["message"] ?>
-                </div>
-              </div>
-            <?php endif;
-            unset($_SESSION["error"]["message"]);
-            ?>
           </div>
         </div>
       </div>
-
-
     </form>
 
     <script>
-      $('input').on('change', function(e) {
-        // console.log(this.files[0]);
-        const file = this.files[0]; //將上傳檔案轉換為base64字串
+      $('input[type="file"]').on('change', function (e) {
+        const file = this.files[0];
+        const fr = new FileReader();
 
-        const fr = new FileReader(); //建立FileReader物件
-
-        fr.onload = function(e) {
-          $('img').attr('src', e.target.result); //讀取的结果放入圖片
+        fr.onload = function (e) {
+          $('img').attr('src', e.target.result);
         };
         $('#deletebtn').show();
-
-        // 使用 readAsDataURL 將圖片轉成 Base64
         fr.readAsDataURL(file);
       });
 
-
-      const deletebtn = document.querySelector("#deletebtn")
-      const imginput = document.querySelector("#imginput")
-      deletebtn.addEventListener("click", function() {
-        // console.log("click");
+      const deletebtn = document.querySelector("#deletebtn");
+      const imginput = document.querySelector("#imginput");
+      deletebtn.addEventListener("click", function (event) {
         event.preventDefault();
         $('img').attr('src', '');
-
-        // deletebtn.type = 'hidden';
-        // 隱藏刪除按鈕
         $('#deletebtn').hide();
         imginput.value = "";
-
-      })
+      });
     </script>
-
-
-
-    <!-- <section>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6 col-md-8">
-
-            <h1 class="text-center">新增使用者</h1>
-            <div class="mb-2">
-              <label for="" class="form-label">名字</label>
-              <input type="text" class="form-control" id="name">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">帳號</label>
-              <input type="text" class="form-control" id="account">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">密碼</label>
-              <input type="password" class="form-control" id="password">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">重新輸入密碼</label>
-              <input type="password" class="form-control" id="repassword">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">信箱</label>
-              <input type="email" class="form-control" id="email">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">生日</label>
-              <input type="date" class="form-control" id="birthday">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">電話號碼</label>
-              <input type="text" class="form-control" id="phone">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">地址</label>
-              <input type="text" class="form-control" id="address">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">信用卡號</label>
-              <input type="text" class="form-control" id="creditcard">
-            </div>
-            <div class="mb-2">
-              <label for="" class="form-label">圖片</label>
-              <input type="file" class="form-control" id="image">
-            </div>
-            <div class="text-center mt-3">
-            <button class="btn btn-primary" type="button" id="send">送出</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> -->
   </main>
-
-  <!-- 
-  <script>
-    const send = document.querySelector("#send"),
-      name = document.querySelector("#name"),
-      account = document.querySelector("#account"),
-      password = document.querySelector("#password"),
-      repassword = document.querySelector("#repassword"),
-      email = document.querySelector("#email"),
-      birthday = document.querySelector("#birthday"),
-      phone = document.querySelector("#phone"),
-      address = document.querySelector("#address"),
-      creditcard = document.querySelector("#creditcard")
-      image=document.querySelector("#image")
-
-    send.addEventListener("click", function () {
-      console.log("click");
-      nameVal = name.value;
-      accountVal = account.value;
-      passwordVal = password.value;
-      repasswordVal = repassword.value;
-      emailVal = email.value;
-      birthdayVal = birthday.value;
-      phoneVal = phone.value;
-      addressVal = address.value;
-      creditcardVal = creditcard.value;
-      imageVal=image.val
-
-
-
-      $.ajax({
-        method:"POST", //or GET
-        url: "../api/doRegister.php",
-        dataType: "json",
-        data: {
-          name: nameVal,
-          account: accountVal,
-          password: passwordVal,
-          repassword: repasswordVal,
-          email: emailVal,
-          birthday: birthdayVal,
-          phone: phoneVal,
-          address: addressVal,
-          creditcard: creditcardVal,
-          image:imageVal
-
-        }
-      })
-        .done(function (response) {
-          // console.log(response);
-          if (response.status == 0) {
-            alert(response.message)
-            return;
-          }
-          console.log(response);
-          alert("新增使用者: " + accountVal + "成功")
-
-        }).fail(function (jqXHR, textStatus) {
-          
-          console.log("Request failed: " + textStatus);
-          
-          console.log("Server response: " + jqXHR.responseText);
-        });
-
-    })
-  </script> -->
-
-
-
-  <!--   Core JS Files   -->
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
 </body>
 
 </html>
+
+<?php
+// 清空 SESSION 中的错误信息
+unset($_SESSION["error"]);
+?>
